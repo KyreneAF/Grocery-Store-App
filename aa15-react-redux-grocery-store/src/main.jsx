5a9c40dc9eff1d4f45aa014237bb91a09d9adccf
Dslayton1998 <dslayton1998@gmail.com>
2023-11-14 16:46:09 -0600

    Finished set-up, ready to start coding

--- aa15-react-redux-grocery-store/src/main.jsx
+++ aa15-react-redux-grocery-store/src/main.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import configureStore from './store/store';
+import { Provider } from 'react-redux';
 import './index.css';
+
+const store = configureStore();
+
+if (import.meta.env.MODE !== "production") {
+  window.store = store;
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Provider store={store}>
       <App />
+    </Provider>
   </React.StrictMode>
 );
 
